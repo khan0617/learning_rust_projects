de6f6142f04f044e28a79c38b224f207660f90f0
Hamza Khan <hamza55014@gmail.com>
2023-04-26 12:04:02 -0500

    finish chapter 3

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,23 +1,79 @@
 use std::io;
 
+fn get_y() -> i32 {
+    {
+        let x = 10;
+        x + 1
+    }
+}
+
+fn five() -> i32 {
+    5
+}
+
 fn main() {
-    let tup: (i32, f64, &str) = (32, 6.4, "Hello!");
-    let a = [1, 2, 3, 4, 5];
+    // for and while loops
+    // let mut number: i32 = 3;
+    // while number != 0 {
+    //     println!("{number}!");
+    //     number -= 1;
+    // }
+    // println!("LIFTOFF!!!");
 
-    println!("Please enter an array index.");
+    // using an inclusive range w/ for loop
+    for number in (1..=4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 
-    let mut index = String::new();
+    // let a: [i32; 5] = [10, 20, 30, 40, 50];
+    // for element in a {
+    //     println!("The value is: {element}");
+    // }
 
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
 
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
+    // ranges and rev
+    // for number in (1..=4).rev() {
+    //     println!("{number}!");
+    // }
+    // println!("LIFTOFF");
 
-    let element = a[index];
+    // function calls and ternary operator
+    // println!("value of y is {}", get_y());
+    // println!("Five: {}", five());
+    // let y = get_y();
+    // let five = five();
+    // let maximum = if y > five { y } else { five };
+    // println!("Maximum is: {maximum}");
 
-    println!("The value of the element at index {index} is: {element}");
+    // loops as expressions
+    // let mut counter = 0;
+    // let result = loop {
+    //     counter += 1;
+    //     if counter == 10 {
+    //         break counter * 10;
+    //     }
+    // };
+    // println!("The result is {result}");
+
+    // variable practice, arrays and tuples
+    // let tup: (i32, f64, &str) = (32, 6.4, "Hello!");
+    // let a = [1, 2, 3, 4, 5];
+
+    // println!("Please enter an array index.");
+
+    // reading from std input
+    // let mut index = String::new();
+    // io::stdin()
+    //     .read_line(&mut index)
+    //     .expect("Failed to read line");
+
+    // let index: usize = index
+    //     .trim()
+    //     .parse()
+    //     .expect("Index entered was not a number");
+
+    // let element = a[index];
+
+    // println!("The value of the element at index {index} is: {element}");
 }
